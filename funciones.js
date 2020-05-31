208fea7b6293eb18b6ad34125b29e3e92393a917
OscarRAmirez <oerm16@gmail.com>
2020-05-31 09:26:58 -0500

    Inicio

---
+++ funciones.js
@@ -0,0 +1,15 @@
+"use strict";
+var saludo = "!!!!TypeScript";
+saludo = "Nuevo Saludo desde TypeScript";
+var numero;
+numero = 10;
+var cualquiera;
+cualquiera = "cadena";
+cualquiera = 10;
+var PI_NUMERO = 3.14;
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+    console.log(PI_NUMERO);
+}
+saludar();
---
+++ funciones.ts
@@ -0,0 +1,19 @@
+let saludo = "!!!!TypeScript";
+saludo = "Nuevo Saludo desde TypeScript";
+
+let numero:number;
+numero = 10;
+
+let cualquiera: any;
+cualquiera = "cadena";
+cualquiera = 10;
+
+const PI_NUMERO = 3.14;
+
+function saludar(){
+    console.log(saludo);
+    console.log(numero);
+    console.log(PI_NUMERO);
+}
+
+saludar();
