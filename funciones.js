952847a17e9041e24abdc48e3f0d21edd840c8ae
OscarRAmirez <oerm16@gmail.com>
2020-05-31 09:56:58 -0500

    Clases

--- funciones.js
+++ funciones.js
@@ -1,15 +1,16 @@
-"use strict";
+'use strict';
 var saludo = "!!!!TypeScript";
 saludo = "Nuevo Saludo desde TypeScript";
-var numero;
+var numero; //let numero = 10;
 numero = 10;
 var cualquiera;
 cualquiera = "cadena";
 cualquiera = 10;
+var espacio;
+espacio = "esto es una prueba";
 var PI_NUMERO = 3.14;
 function saludar() {
-    console.log(saludo);
-    console.log(numero);
+    console.log(saludo, numero);
     console.log(PI_NUMERO);
 }
 saludar();
--- funciones.ts
+++ funciones.ts
@@ -1,18 +1,23 @@
+'use strict';
 let saludo = "!!!!TypeScript";
 saludo = "Nuevo Saludo desde TypeScript";
 
-let numero:number;
+
+let numero:number;//let numero = 10;
 numero = 10;
 
 let cualquiera: any;
 cualquiera = "cadena";
 cualquiera = 10;
 
+let espacio: string;
+
+espacio="esto es una prueba";
+
 const PI_NUMERO = 3.14;
 
 function saludar(){
-    console.log(saludo);
-    console.log(numero);
+    console.log(saludo,numero);
     console.log(PI_NUMERO);
 }
 
