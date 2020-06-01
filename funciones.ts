f925a101e8ce7b0f8d841c3b1fa38a45b0979cdb
OscarRAmirez <oerm16@gmail.com>
2020-06-01 08:03:38 -0500

    actualizacion

--- funciones.ts
+++ funciones.ts
@@ -1,24 +1,34 @@
-'use strict';
 let saludo = "!!!!TypeScript";
 saludo = "Nuevo Saludo desde TypeScript";
 
+let variable: string | number;
 
-let numero:number;//let numero = 10;
+
+
+
+let numero: number;//let numero = 10;
 numero = 10;
 
 let cualquiera: any;
 cualquiera = "cadena";
 cualquiera = 10;
 
-let espacio: string;
-
-espacio="esto es una prueba";
-
 const PI_NUMERO = 3.14;
 
-function saludar(){
-    console.log(saludo,numero);
+function saludar() {
+    console.log(saludo, numero);
     console.log(PI_NUMERO);
 }
+
+let spiderman{//declarando objeto****
+    nombre = "Jose",
+    Edad = 30
+}
+spiderman={
+
+    nombre = "Reasignado",
+    Edad = 50
+}
+
 
 saludar();
